1c69d483e15b74371fe96c20e02ea90bb07ac010
Roger Espinoza <rj.espinoza44@gmail.com>
2022-12-11 23:20:59 -0500

    Initial Commit

---
+++ I Am Rich/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  I Am Rich
+//
+//  Created by Roger Espinoza on 11/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
